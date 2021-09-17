ba27508775e182831650b09026d7508627f6442b
Jonathan Quirino <75098397+jonathan619@users.noreply.github.com>
2021-09-16 22:18:16 -0500

    inverted colors in camera-test.py

--- src/camera-test.py
+++ src/camera-test.py
@@ -1,7 +1,12 @@
 import numpy as np
 import cv2
 
+
+def apply_invert(frame):
+    return cv2.bitwise_not(frame)
+
 cap = cv2.VideoCapture(0)
+
 
 while(True):
     # Capture frame-by-frame
@@ -9,10 +14,12 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    inverted = apply_invert(frame)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
     cv2.imshow('gray',gray)
+    cv2.imshow('inverted', inverted)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
