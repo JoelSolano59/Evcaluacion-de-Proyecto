1c276b063f426bc7a6930d77c560b42bd00d9adc
Jonathan Quirino <75098397+jonathan619@users.noreply.github.com>
2021-09-16 22:37:57 -0500

    calando y arreglando el de watermark.py

--- src/watermark.py
+++ src/watermark.py
@@ -5,14 +5,14 @@ from utils import CFEVideoConf, image_resize
 
 cap = cv2.VideoCapture(0)
 
-save_path = 'saved-media/watermark.mp4'
+##save_path = 'saved-media/watermark.mp4'
 frames_per_seconds = 24
-config = CFEVideoConf(cap, filepath=save_path, res='720p')
-out = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
+##config = CFEVideoConf(cap, filepath=save_path, res='720p')
+##out = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
 
-img_path = 'images/logo/cfe-coffee.png'
+img_path = 'tec.png'
 logo = cv2.imread(img_path, -1)
-watermark = image_resize(logo, height=50)
+watermark = image_resize(logo, height=100)
 watermark = cv2.cvtColor(watermark, cv2.COLOR_BGR2BGRA)
 
 while(True):
@@ -27,7 +27,7 @@ while(True):
     for i in range(0, watermark_h):
         for j in range(0, watermark_w):
             if watermark[i,j][3] != 0:
-                offset = 10
+                offset = 30
                 h_offset = frame_h - watermark_h - offset
                 w_offset = frame_w - watermark_w - offset
                 overlay[h_offset + i, w_offset+ j] = watermark[i,j]
@@ -35,7 +35,7 @@ while(True):
     cv2.addWeighted(overlay, 0.25, frame, 1.0, 0, frame)
 
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-    out.write(frame)
+    ##out.write(frame)
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
@@ -43,5 +43,5 @@ while(True):
 
 # When everything done, release the capture
 cap.release()
-out.release()
+##out.release()
 cv2.destroyAllWindows()
